f763361f161120249af376745309a7773ad8e021
ArpanSamariya <arpansamria16@gmail.com>
2017-06-24 17:36:04 +0530

    df

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var cors= require('cors');
+var methodOverride= require('method-override')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +23,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((methodOverride));
+app.use((cors));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    res.header('Access-Control-Allow-Methods', 'PUT,  DELETE');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,14 +45,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 /// cros
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-   // res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
