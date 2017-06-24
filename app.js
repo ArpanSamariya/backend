4366e7fa663c608ebde051db9143b8da40e91123
ArpanSamariya <arpansamria16@gmail.com>
2017-06-24 17:49:22 +0530

    ffdw

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((methodOverride));
-app.use((cors));
+app.use((methodOverride()));
+app.use((cors()));
 
 app.use('/', index);
 app.use('/users', users);
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 /// cros
-app.all('*', function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
