04243a392bcf77e37c2fd9a1457740a15d73c0c7
ArpanSamariya <arpansamria16@gmail.com>
2017-06-24 12:46:15 +0530

    dgffdg

--- app.js
+++ app.js
@@ -33,9 +33,11 @@ app.use(function(req, res, next) {
 });
 /// cros
 app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+   // res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 // error handler
